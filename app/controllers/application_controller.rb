dd9b918b6b9f5ef6cde9581bada7c22eb0397e62
Yuma Tsukumo <yuma@iMac.local>
2020-12-28 01:21:09 +0900

    setting strong parameter at user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    # deviseで承認するカラムの設定
+    def configure_permitted_parameters
+      added_attrs = [ :email, :name, :password, :password_confirmation, :username, :introduce, :image ]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    end
 end
