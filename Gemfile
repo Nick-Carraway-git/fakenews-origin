a3fb391c937efa6ea3daf8c532d01c0792735ae4
Yuma Tsukumo <yuma@iMac.local>
2021-01-06 06:14:21 +0900

    make basic article-image  validate

--- Gemfile
+++ Gemfile
@@ -55,6 +55,9 @@ gem 'jquery-rails'
 # font-awesome
 gem 'font-awesome-sass'
 
+# Active Storage
+gem 'active_storage_validations'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,12 @@ class Article < ApplicationRecord
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 70 }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                    message: "must be a valid image format" },
+                    size:         { less_than: 3.megabytes,
+                                    message: "should be less than 3MB" }
 
   # 中間テーブルのための関連づけ
   has_many :article_categories, dependent: :destroy
