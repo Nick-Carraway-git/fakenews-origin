fefd0d6df11e3b77224b55ff1766e8867093b9bc
Yuma Tsukumo <yuma@iMac.local>
2021-01-10 09:08:25 +0900

    make basic user-show page layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     return redirect_to request.referrer || root_url if @user.blank?
     @articles = Article.where(user_id: @user.id)
-    @boards = Boardroom.where(user_id: @user.id)
+    @boards = @user.boardrooms
   end
 end
--- db/migrate/20210109215706_add_column_to_boardroom.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToBoardroom < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :boardrooms, :user, foreign_key: true
-  end
-end
