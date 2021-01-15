991928f33e7819fb57110475d044f7002f177f15
Yuma Tsukumo <yuma@iMac.local>
2021-01-15 16:07:30 +0900

    rewrite chat validation

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,8 +1,4 @@
 class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :boardroom, dependent: :destroy
-
-  validates :message, presence: true
-  validates :user_id, presence: true
-  validates :boardroom_id, presence: true
 end
