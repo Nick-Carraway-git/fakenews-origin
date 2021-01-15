91efe65447b14a8169473a6a0b9fd3d78f9d4804
Yuma Tsukumo <yuma@iMac.local>
2021-01-15 14:53:49 +0900

    rewrite chat page

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,7 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :boardroom, dependent: :destroy
 
-  validates :message, presence: true, length: { maximum: 120 }
+  validates :message, presence: true
   validates :user_id, presence: true
   validates :boardroom_id, presence: true
 end
