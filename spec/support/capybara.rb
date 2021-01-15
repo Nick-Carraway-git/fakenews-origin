553fb61300546591df55e170537ba6e8d1a6ae5a
Yuma Tsukumo <yuma@iMac.local>
2021-01-15 13:09:10 +0900

    make capybara puppot file

---
+++ spec/support/capybara.rb
@@ -0,0 +1,15 @@
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium, using: :headless_chrome, options: {
+      browser: :remote,
+      url: ENV.fetch("SELENIUM_DRIVER_URL"),
+      desired_capabilities: :chrome,
+    }
+    Capybara.server_host = 'fakenews'
+    Capybara.app_host = 'http://fakenews'
+  end
+end
