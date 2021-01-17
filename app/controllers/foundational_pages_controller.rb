5d23c0562463d3e67c912bf0e60292de6c310b87
Yuma Tsukumo <yuma@iMac.local>
2021-01-17 14:09:57 +0900

    make random

--- app/controllers/foundational_pages_controller.rb
+++ app/controllers/foundational_pages_controller.rb
@@ -1,12 +1,8 @@
 class FoundationalPagesController < ApplicationController
   def index
     @articles = Article.distinct.where(created_at: Time.now - 3.days...Time.now).sample(MAX_SLIDE_NUM)
-    # @articles = Article.distinct.where(created_at: Time.now - 3.days...Time.now).order("RAND()").limit(MAX_SLIDE_NUM)
-    # TOCO(ranker用処理)
-    @rankers = User.all.sample(5)
-    # @rankers.each do |ranker|
-    #    ranker.articles.count * ranker.favorited.count
-    #  end
+    @rankers = User.joins(:articles).group(:id).order("count(articles.user_id) DESC")
+                   .limit(5).shuffle
   end
 
   def help
--- spec/factories/boardrooms.rb
+++ spec/factories/boardrooms.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :boardroom do
     name { "MyString" }
-    article { nil }
+    article
   end
 end
